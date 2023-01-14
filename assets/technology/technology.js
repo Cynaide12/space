fe1c59f037c15e4336c70a012c9858dc9c56d1d8
Cynaide12 <114859811+Cynaide12@users.noreply.github.com>
2023-01-14 20:53:49 +0500

    Update technology.js

--- assets/technology/technology.js
+++ assets/technology/technology.js
@@ -1,26 +1,26 @@
-const stage = document.querySelector('.name-stage')
-const description = document.querySelector('.description')
-const button = document.querySelectorAll('.button-number')
-const img = document.querySelector('.container-img img')
-let active = document.querySelector('.button-active')
-import data from '../../data.json' assert {type: 'json'}
-let technology = data['technology']
-for(let i = 0; i < button.length; i++){
-  function getImg(){
-    if(window.innerWidth >= 1000){
-      return technology[i]['images']['portrait']
+const stage = document.querySelector(".name-stage");
+const description = document.querySelector(".description");
+const button = document.querySelectorAll(".button-number");
+const portrait = document.querySelector("#portrait");
+const landscape = document.querySelector("#landscape");
+let active = document.querySelector(".button-active");
+import data from "../../data.json" assert { type: "json" };
+let technology = data["technology"];
+for (let i = 0; i < button.length; i++) {
+  function getImg() {
+    if (window.innerWidth >= 1000) {
+      return technology[i]["images"]["portrait"];
     }
-    return technology[i]['images']['landscape']
+    return technology[i]["images"]["landscape"];
   }
-  let btn = button[i]
-  btn.addEventListener('click', () => {
-    stage.textContent = technology[i]['name']
-    description.textContent = technology[i]['description']
-    img.src = getImg()
-    active = document.querySelector('.button-active')
-    active.classList.remove('button-active')
-    btn.classList.add('button-active')
-  })
-
+  let btn = button[i];
+  btn.addEventListener("click", () => {
+    stage.textContent = technology[i]["name"];
+    description.textContent = technology[i]["description"];
+    portrait.src = getImg();
+    landscape.src = getImg();
+    active = document.querySelector(".button-active");
+    active.classList.remove("button-active");
+    btn.classList.add("button-active");
+  });
 }
-
